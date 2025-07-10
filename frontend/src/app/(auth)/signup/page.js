cf93e6312f037e4014d999ae40aaa7148075e925
Elaya1220 <elayaraja1220@outlook.com>
2025-07-10 18:25:54 +0530

    floatinglabel input email password

--- frontend/src/app/(auth)/signup/page.js
+++ frontend/src/app/(auth)/signup/page.js
@@ -1,116 +1,101 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { InputCheckBox, InputEmail, InputPassword, InputText } from '@/app/components/form/formList';
+
+import {
+  InputCheckBox,
+  InputEmail,
+  InputPassword,
+  InputText
+} from '@/app/components/form/formList';
+
 import Form from '@/app/components/form/form/form';
 import patterns from '@/app/utils/regx/regx';
-
 
 const schema = yup.object({
   firstName: yup.string()
     .required('First name is required')
-    .min(3, 'Minimum 3 charateer must'),
-  lastname: yup.string()
-    .notRequired(),
+    .min(3, 'Minimum 3 characters required'),
+  lastname: yup.string().notRequired(),
   gender: yup.string()
     .required('Gender is required')
     .oneOf(['male', 'female', 'other'], 'Gender must be male, female, or other'),
-
   password: yup.string()
     .required('Password is required')
-    .matches(patterns.strongPassword, 'Password must be at least 8 characters, with upper, lower, number, and one special character (@, $, !, %, *, ?, &)'),
+    .matches(
+      patterns.strongPassword,
+      'Password must be at least 8 characters, with upper, lower, number, and one special character (@, $, !, %, *, ?, &)'
+    ),
   email: yup.string()
-    .required('Email  is required')
+    .required('Email is required')
     .matches(patterns.email, 'Invalid email format. Example: abc@gmail.com')
-})
+});
 
 export default function SignupForm() {
+  const methods = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+  } = methods;
 
   const onSubmit = (data) => {
     console.log('Form Submitted:', data);
   };
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <InputText
-      size="md"
-        label="First Name"
-        name="firstName"
-        placeholder="Enter your first name"
-        register={register}
-        error={errors.firstName?.message}
-        id="firstname"
-        formGroupLayout='vertical'
-        errorMessageLayout='vertical'
-        variant = 'filled'
-        floatLabel={true}
-      />
-      {/* <InputPassword
-        id="Password"
-        size="md"
-        label="Password"
-        name="password"
-        placeholder="Enter your Password"
-        register={register}
-        error={errors.password?.message}
-        toggleMask
-        formGroupLayout='vertical'
-        errorMessageLayout='vertical'
-        floatLabel={false}
-      />
-      <InputEmail
-        id="email"
-        size="md"
-        label="Email"
-        name="email"
-        placeholder="Enter your email address"
-        register={register}
-        error={errors.email?.message}
-        formGroupLayout='vertical'
-        errorMessageLayout='vertical'
-        floatLabel={false}
-      /> */}
-       {/* <InputCheckBox
-        id="checkbox"
-        size="md"
-        label="checkbox"
-        name="checkbox"
-        placeholder="Enter your checkbox"
-        register={register}
-        error={errors.checkbox?.message}
-        formGroupLayout='vertical'
-        errorMessageLayout='vertical'
-        floatLabel={false}
-        labelPosition = "right"
-      />
-       <InputCheckBox
-        id="checkbox"
-        size="md"
-        label="checkbox"
-        name="checkbox"
-        placeholder="Enter your checkbox"
-        register={register}
-        error={errors.checkbox?.message}
-        formGroupLayout='vertical'
-        errorMessageLayout='vertical'
-        floatLabel={false}
-        labelPosition = "right"
-      /> */}
-      {/* <button
-        type="submit"
-        className=""
-      >
-        Submit
-      </button> */}
-    </Form>
+    <FormProvider {...methods}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputText
+          size="md"
+          label="First Name"
+          name="firstName"
+          placeholder="Enter your first name"
+          register={register}
+          error={errors.firstName?.message}
+          id="firstname"
+          formGroupLayout="vertical"
+          errorMessageLayout="vertical"
+          variant="outlined"
+          floatLabel
+        />
+
+        <InputPassword
+          size="md"
+          label="Password"
+          name="password"
+          placeholder="Enter your password"
+          register={register}
+          error={errors.password?.message}
+          id="password"
+          formGroupLayout="vertical"
+          errorMessageLayout="vertical"
+          variant="outlined"
+          floatLabel
+          toggleMask
+        />
+
+        <InputEmail
+          size="md"
+          label="Email"
+          name="email"
+          placeholder="Enter your Email"
+          register={register}
+          error={errors.email?.message}
+          id="email"
+          formGroupLayout="vertical"
+          errorMessageLayout="vertical"
+          variant="outlined"
+          floatLabel
+        />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </FormProvider>
   );
 }
